49c08d226589f8ccb020eab626081ce8c10bacc9
Artem Efimenko <aefimenko@agilevision.io>
2018-03-28 10:01:37 +0300

    Delete echo

--- npm-publish.sh
+++ npm-publish.sh
@@ -3,9 +3,6 @@ VERSION=`cat package.json | underscore select '.version' --outfmt text`
 PACK_NAME=`cat package.json | underscore select '.name' --outfmt text`
 NPM_VERSION=`npm view $PACK_NAME version`
 
-echo $PACK_NAME
-echo $VERSION
-echo $NPM_VERSION
 if [ "$VERSION" = "$NPM_VERSION" ]
 then
     echo "Package $PACK_NAME does not need to be updated"
