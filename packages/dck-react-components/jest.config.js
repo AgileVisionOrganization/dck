cda8b688bcaf8d108d50603b860aab33e1795049
Artem Efimenko <aefimenko@agilevision.io>
2018-04-30 16:48:33 +0300

    Implement datetimepicker component

--- packages/dck-react-components/jest.config.js
+++ packages/dck-react-components/jest.config.js
@@ -3,6 +3,9 @@ module.exports = {
   transform: {
     "\\.(ts|tsx)$": "./node_modules/ts-jest/preprocessor.js"
   },
+  moduleNameMapper: {
+    "\\.(css|less)$": "identity-obj-proxy"
+  },
   testRegex: ".*\\.spec\\.(ts|tsx|js)$",
   testResultsProcessor: "./node_modules/jest-junit-reporter"
 };
--- packages/dck-react-components/src/components/FieldGroup.tsx
+++ packages/dck-react-components/src/components/FieldGroup.tsx
@@ -1,16 +1,36 @@
 import * as React from "react";
-import * as ControlLabel from "react-bootstrap/lib/ControlLabel";
-import * as FormControl from "react-bootstrap/lib/FormControl";
-import * as Checkbox from "react-bootstrap/lib/Checkbox";
-import * as FormGroup from "react-bootstrap/lib/FormGroup";
-import * as HelpBlock from "react-bootstrap/lib/HelpBlock";
-import * as InputGroup from "react-bootstrap/lib/InputGroup";
+import {
+  ControlLabel,
+  FormControl,
+  Checkbox,
+  FormGroup,
+  HelpBlock,
+  InputGroup,
+} from "react-bootstrap";
 import * as FontAwesome from "react-fontawesome";
-import * as DateTime from "react-datetime";
+import * as ReactDatetime from "react-datetime"
+import "../../node_modules/react-datetime/css/react-datetime.css"
 
-export type FieldInputType = "text" | "password" | "email" | "checkbox" | "datepicker";
+export type FieldInputType =
+  | "text"
+  | "password"
+  | "email"
+  | "checkbox"
+  | "datetimepicker"
+  | "datepicker"
+  | "timepicker";
 
-export interface FieldGroupProps {
+export const InputTypes = Object.freeze({
+  text: "text",
+  password: "password",
+  email: "email",
+  checkbox: "checkbox",
+  datetimepicker: "datetimepicker",
+  datepicker: "datepicker",
+  timepicker: "timepicker",
+});
+
+export interface IFieldGroupInputProps {
   type?: FieldInputType;
   placeholder?: string;
   id?: string;
@@ -20,12 +40,56 @@ export interface FieldGroupProps {
   validationMessage?: string;
   onFocus?: () => void;
   onChange?: (e: any) => void;
-  value?: any;
 }
 
-export class FieldGroup extends React.Component<FieldGroupProps, any> {
+export interface IFieldGroupSelectProps {
+  selectClass?: string;
+  value?: any;
+  arrowIconUp?: string;
+  arrowIconDown?: string;
+  arrowsSize?: FontAwesome.FontAwesomeSize;
+  arrowContainerClass?: string;
+  clearable?: boolean;
+  searchable?: boolean;
+  multi?: boolean;
+  selectValues?: any[];
+}
+
+export interface IFieldGroupDateTimeProps {
+  dateTimeClass?: string;
+
+  dateFormat?: boolean|string;
+  timeFormat?: boolean|string;
+  input?: boolean;
+  open?: boolean;
+  locale?: string;
+  utc?: boolean;
+  onBlur?: (e: any) => void;
+  inputProps?: Object;
+  strictParsing?: boolean;
+  closeOnSelect?: boolean;
+  closeOnTab?: boolean;
+  disableOnClickOutside?: boolean; 
+}
+
+export interface IFieldGroupProps extends IFieldGroupInputProps, IFieldGroupSelectProps, IFieldGroupDateTimeProps {}
+
+export class FieldGroup extends React.Component<IFieldGroupProps, any> {
   public static defaultProps = {
     type: "text",
+    selectClass: "select-class",
+    clearable: false,
+    multi: false,
+    searchable: false,
+    arrowIconUp: "angle-up",
+    arrowIconDown: "angle-down",
+    arrowContainerClass: "arrow-container",
+    dateTimeClass: "date-time-class",
+    closeOnSelect: true,
+    disableOnClickOutside: false,
+    open: false,
+    input: true,
+    defaultValue: new Date()
   };
 
   public getValidationState(validation: any) {
@@ -47,20 +111,7 @@ export class FieldGroup extends React.Component<FieldGroupProps, any> {
         validationState={this.getValidationState(this.props.validationState)}
       >
         <ControlLabel>{this.props.label}</ControlLabel>
-        {this.props.type === "checkbox"? 
-
-        <Checkbox value={this.props.value} onChange={this.props.onChange} /> :
-        
-        <FormControl
-          onFocus={this.props.onFocus}
-          onChange={this.props.onChange}
-          type={this.props.type}
-          placeholder={this.props.placeholder}
-          value={this.props.value}
-        />
-        }
-        
-        {" "}
+        {this.getCurrentRender()}
         {this.props.help && <HelpBlock>{this.props.help}</HelpBlock>}
         {this.props.validationMessage &&
         this.props.validationState &&
@@ -73,6 +124,71 @@ export class FieldGroup extends React.Component<FieldGroupProps, any> {
           <HelpBlock>&nbsp;</HelpBlock>
         )}
       </FormGroup>
+    );
+  }
+
+  private getCurrentRender() {
+    let render = null;
+    switch (this.props.type) {
+      case InputTypes.checkbox:
+        render = this.renderCheckBox();
+        break;
+      case InputTypes.datetimepicker:
+        render = this.renderDateTimePicker();
+        break;
+      case InputTypes.datepicker:
+        render = this.renderDatePicker();
+        break;
+      case InputTypes.timepicker:
+        render = this.renderTimePicker();
+        break;
+      default:
+        render = this.renderTextInput();
+        break;
+    }
+
+    return render;
+  }
+
+  private renderDateTimePicker() {
+    return (
+      <div className={this.props.dateTimeClass}>
+        <ReactDatetime {...this.props}/> 
+      </div>
+    );
+  }
+
+  private renderDatePicker() {
+    const {dateFormat, timeFormat, ...otherProps} = this.props;
+    return (
+      <div className={this.props.dateTimeClass}>
+        <ReactDatetime dateFormat={dateFormat ? dateFormat : true} timeFormat={false} {...otherProps}/>
+      </div>
+    );
+  }
+
+  private renderTimePicker() {
+    const {dateFormat, timeFormat, ...otherProps} = this.props;
+    return (
+      <div className={this.props.dateTimeClass}>
+        <ReactDatetime dateFormat={false} timeFormat={timeFormat ? timeFormat : true} {...otherProps}/>
+      </div>
+    );
+  }
+
+  private renderCheckBox() {
+    return <Checkbox value={this.props.value} onChange={this.props.onChange} />;
+  }
+
+  private renderTextInput() {
+    return (
+      <FormControl
+        onFocus={this.props.onFocus}
+        onChange={this.props.onChange}
+        type={this.props.type}
+        placeholder={this.props.placeholder}
+        value={this.props.value}
+      />
     );
   }
 }
