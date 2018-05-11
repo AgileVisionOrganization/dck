b6f05e86430d54fc2ed863d5d01ea235c2ccef7a
Artem Yefimenko <artem7902@yandex.ru>
2018-05-11 10:08:46 +0300

    Feature/157148794 auto generated mappers (#24)
    * #157148794: WIP - autogenerated mappings
    
    * WIP: Add specs
    
    * add tests for props mappings
    
    * package-lock.json were changed
    
    * resolve package-lock.json conflicts

---
+++ packages/dck-redux/src/utils.spec.ts
@@ -0,0 +1,154 @@
+import { fromJS } from "immutable";
+import { stateToPropsMappingsForItem, dispatchToPropsMappingsForItem, createReducer } from "./utils";
+import * as types from "./actions/types";
+import { createStore } from "redux";
+
+const TEST_ITEM_TYPE = "TestItem";
+
+describe("Utils Tests", () => {
+    describe("stateToPropsMappingsForItem", () => {
+        it("should create proper mappings", () => {
+            const state = {
+                dck: {
+                    items: fromJS({
+                        TestItem: {
+                            items: [],
+                            selected: [],
+                            active: null,
+                            term: "",
+                            filters: {},
+                            sortingOptions: []
+                        }
+                    }),
+                    processes: fromJS({
+                        TESTITEM_LOAD: {
+                            running: true,
+                            failed: false
+                        },
+                        TESTITEM_ADD: {
+                            running: false,
+                            failed: true
+                        },
+                        TESTITEM_UPDATE: {
+                            running: false,
+                            failed: false
+                        },
+                        TESTITEM_REMOVE: {
+                            running: false,
+                            success: true
+                        }
+                    })
+                }
+            };
+
+            const mappings = stateToPropsMappingsForItem(state, TEST_ITEM_TYPE);
+
+            expect(mappings).toBeDefined();
+
+            expect(mappings).toHaveProperty("testItemsLoading");
+            expect(mappings).toHaveProperty("testItemsLoadFailed");
+            expect(mappings).toHaveProperty("testItemsLoadSuccess");
+
+            expect(mappings.testItemsLoading).toBeTruthy();
+            expect(mappings.testItemsLoadFailed).toBeFalsy();
+            expect(mappings.testItemsLoadSuccess).toBeFalsy();
+
+            expect(mappings).toHaveProperty("testItemAdding");
+            expect(mappings).toHaveProperty("testItemAddFailed");
+            expect(mappings).toHaveProperty("testItemAddSuccess");
+
+            expect(mappings.testItemAdding).toBeFalsy();
+            expect(mappings.testItemAddFailed).toBeTruthy();
+            expect(mappings.testItemsLoadSuccess).toBeFalsy();
+
+            expect(mappings).toHaveProperty("testItemUpdating");
+            expect(mappings).toHaveProperty("testItemUpdateFailed");
+            expect(mappings).toHaveProperty("testItemUpdateSuccess");
+
+            expect(mappings.testItemUpdating).toBeFalsy();
+            expect(mappings.testItemUpdateFailed).toBeFalsy();
+            expect(mappings.testItemUpdateSuccess).toBeFalsy();
+
+            expect(mappings).toHaveProperty("testItemRemoving");
+            expect(mappings).toHaveProperty("testItemRemoveFailed");
+            expect(mappings).toHaveProperty("testItemRemoveSuccess");
+
+            expect(mappings.testItemRemoving).toBeFalsy();
+            expect(mappings.testItemRemoveFailed).toBeFalsy();
+            expect(mappings.testItemRemoveSuccess).toBeTruthy();
+        });
+    });
+    describe("dispatchToPropsMappingsForItem", () => {
+        it("should create proper mappings", () => {
+            
+            const initialState: any = {testItems: [{id: "testId"}]};
+          
+            const testReducer =  createReducer(initialState, {
+                [types.ITEMS_LOAD](state: any) {
+                  return state;
+                },
+                [types.ITEM_ADD](state: any, action: any) {
+                  state.testItems.push(action.data);
+                  return state;
+                },
+                [types.ITEM_SAVE](state: any, action: any) {
+                  state.testItems.map((item:any, index:any)=>{ if(item.id===action.id){ state.testItems[index] = action.data } })
+                  return state;
+                },
+                [types.ITEM_REMOVE](state: any, action: any) {
+                  state.testItems=state.testItems.filter((item:any)=> item.id!==action.id);
+                  return state;
+                },
+                [types.ITEMS_REMOVE](state: any, action: any) {
+                  state.testItems=state.testItems.filter((item:any)=>{
+                      return action.ids.filter((id:any) => id===item.id).length===0;
+                   });
+                  return state;
+                }
+            });
+
+            const store = createStore(testReducer);
+
+            const mappings = dispatchToPropsMappingsForItem(store.dispatch, TEST_ITEM_TYPE);
+
+            expect(mappings).toBeDefined();
+
+            expect(mappings).toHaveProperty("loadTestItems");
+            expect(mappings).toHaveProperty("addTestItem");
+            expect(mappings).toHaveProperty("updateTestItem");
+            expect(mappings).toHaveProperty("removeTestItem");
+            expect(mappings).toHaveProperty("removeTestItems");
+
+            const loadTestItemsAction = mappings.loadTestItems();
+            expect(loadTestItemsAction).toBeDefined();
+            expect(loadTestItemsAction.type).toEqual('ITEMS_LOAD');
+            expect(loadTestItemsAction.itemType).toEqual('TestItem');
+            expect(store.getState().testItems).toEqual([ { id: 'testId' } ]);
+
+            const addTestItemAction = mappings.addTestItem({id: "testId2"});
+            expect(addTestItemAction).toBeDefined();
+            expect(addTestItemAction.type).toEqual('ITEM_ADD');
+            expect(addTestItemAction.itemType).toEqual('TestItem');
+            expect(store.getState().testItems).toEqual([ { id: 'testId' }, { id: 'testId2' } ]);
+
+            const updateTestItemAction = mappings.updateTestItem("testId2", {id: "testId3"});
+            expect(updateTestItemAction).toBeDefined();
+            expect(updateTestItemAction.type).toEqual('ITEM_SAVE');
+            expect(updateTestItemAction.itemType).toEqual('TestItem');
+            expect(store.getState().testItems).toEqual([ { id: 'testId' }, { id: 'testId3' } ] );
+
+            const deleteTestItemAction = mappings.removeTestItem("testId3");
+            expect(deleteTestItemAction).toBeDefined();
+            expect(deleteTestItemAction.type).toEqual('ITEM_REMOVE');
+            expect(deleteTestItemAction.itemType).toEqual('TestItem');
+            expect(store.getState().testItems).toEqual([ { id: 'testId' } ] );
+
+            const deleteTestItemsAction = mappings.removeTestItems(["testId"]);
+            expect(deleteTestItemsAction).toBeDefined();
+            expect(deleteTestItemsAction.type).toEqual('ITEMS_REMOVE');
+            expect(deleteTestItemsAction.itemType).toEqual('TestItem');
+            expect(store.getState().testItems).toEqual([] );
+
+        });
+    });
+});
--- packages/dck-redux/src/utils.ts
+++ packages/dck-redux/src/utils.ts
@@ -1,3 +1,17 @@
+import { plural } from 'pluralize';
+
+import { selectAllItems, selectActiveItem, selectProcessFailed, selectProcessSuccess, selectProcessRunning, selectProcess } from './selectors';
+import { DckActionCreators } from "./actions";
+
+function capitalizeFirstLetter(s: string) {
+  return s.charAt(0).toUpperCase() + s.slice(1);
+}
+
+function lowercaseFirstLetter(s: string) {
+    return s.charAt(0).toLocaleLowerCase() + s.slice(1);
+}
+
+
 export function createReducer(initialState: any, handlers: any) {
   return function reducer(state = initialState, action: any) {
     if (handlers.hasOwnProperty(action.type)) {
@@ -6,4 +20,59 @@ export function createReducer(initialState: any, handlers: any) {
       return state;
     }
   };
+}
+
+
+export function stateToPropsMappingsForItem(state: any, itemType: string) {
+  
+  const pluralLowercase = lowercaseFirstLetter(plural(itemType));
+  const pluralCapitalized = capitalizeFirstLetter(plural(itemType));
+  const singularCapitalized = capitalizeFirstLetter(itemType);
+  const singularLowercase = lowercaseFirstLetter(itemType);
+
+  const loadProcessName = `${itemType.toUpperCase()}_LOAD`;
+  const addProcessName = `${itemType.toUpperCase()}_ADD`;
+  const updateProcessName = `${itemType.toUpperCase()}_UPDATE`;
+  const removeProcessName = `${itemType.toUpperCase()}_REMOVE`;
+  
+
+
+  const mappings : any = {};
+  mappings[pluralLowercase] = selectAllItems(state, itemType);
+  mappings[`current${singularCapitalized}`] = selectActiveItem(state, itemType);
+  
+  mappings[`${pluralLowercase}Loading`] = selectProcessRunning(state, loadProcessName);
+  mappings[`${pluralLowercase}LoadFailed`] = selectProcessFailed(state, loadProcessName);
+  mappings[`${pluralLowercase}LoadSuccess`] = selectProcessSuccess(state, loadProcessName);
+
+  mappings[`${singularLowercase}Adding`] = selectProcessRunning(state, addProcessName);
+  mappings[`${singularLowercase}AddFailed`] = selectProcessFailed(state, addProcessName);
+  mappings[`${singularLowercase}AddSuccess`] = selectProcessSuccess(state, addProcessName);
+
+  mappings[`${singularLowercase}Updating`] = selectProcessRunning(state, updateProcessName);
+  mappings[`${singularLowercase}UpdateFailed`] = selectProcessFailed(state, updateProcessName);
+  mappings[`${singularLowercase}UpdateSuccess`] = selectProcessSuccess(state, updateProcessName);
+
+  mappings[`${singularLowercase}Removing`] = selectProcessRunning(state, removeProcessName);
+  mappings[`${singularLowercase}RemoveFailed`] = selectProcessFailed(state, removeProcessName);
+  mappings[`${singularLowercase}RemoveSuccess`] = selectProcessSuccess(state, removeProcessName);
+  
+  return mappings;
+}
+
+export function dispatchToPropsMappingsForItem(dispatch: any, itemType: string) {
+  const pluralLowercase = plural(itemType).toLocaleLowerCase();
+  const pluralCapitalized = capitalizeFirstLetter(plural(itemType));
+  const singularCapitalized = capitalizeFirstLetter(itemType);
+  const singularLowercase = itemType.toLowerCase();
+  const mappings : any = {};
+
+  mappings[`load${pluralCapitalized}`] = (filteringOptions: any) => dispatch(DckActionCreators.itemsLoad(itemType, filteringOptions));
+  mappings[`add${singularCapitalized}`] = (data: any) => dispatch(DckActionCreators.itemAdd(itemType, data));
+  mappings[`update${singularCapitalized}`] = (id: any, data: any) => dispatch(DckActionCreators.itemSave(itemType, id, data));
+  mappings[`remove${singularCapitalized}`] = (id: any) => dispatch(DckActionCreators.itemRemove(itemType, id));
+  mappings[`remove${pluralCapitalized}`] = (ids: any[]) => dispatch(DckActionCreators.itemsRemove(itemType, ids));
+  
+
+  return mappings;
 }
