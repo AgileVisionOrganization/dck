020db762726d8ddd38183cfb05de0df1a7f5ca21
Artem Efimenko <aefimenko@agilevision.io>
2018-06-08 12:02:51 +0300

    Update travis config

--- npm-publish.sh
+++ npm-publish.sh
@@ -3,7 +3,7 @@ VERSION=`cat package.json | underscore select '.version' --outfmt text`
 PACK_NAME=`cat package.json | underscore select '.name' --outfmt text`
 NPM_VERSION=`npm view $PACK_NAME version`
 
-if [ "$VERSION" = "$NPM_VERSION" ]
+if [ "$VERSION" = "$NPM_VERSION" ] || [ "$TRAVIS_BRANCH" -ne "master" ]
 then
     echo "Package $PACK_NAME does not need to be updated"
 else 
