2cfc9f10586a502a8b41df715714455918a9b0f9
Artem Efimenko <aefimenko@agilevision.io>
2018-07-25 12:05:45 +0300

    Build/fix (#38)
    * Add dependent fields support to dck-react-components utils
    
    * Bump version
    
    * Change build script for update version only in master

--- npm-publish.sh
+++ npm-publish.sh
@@ -3,7 +3,7 @@ VERSION=`cat package.json | underscore select '.version' --outfmt text`
 PACK_NAME=`cat package.json | underscore select '.name' --outfmt text`
 NPM_VERSION=`npm view $PACK_NAME version`
 
-if [ $VERSION -gt $NPM_VERSION ] && [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_EVENT_TYPE" != "push" ]
+if [ "$VERSION" != "$NPM_VERSION" ] && [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_EVENT_TYPE" != "push" ]
 then
     npm publish
 else 
