4b6e626e14955b8de243ba2b98b15d7380f0d356
Artem Efimenko <aefimenko@agilevision.io>
2018-07-25 12:16:06 +0300

    Fix build #1

--- npm-publish.sh
+++ npm-publish.sh
@@ -2,8 +2,7 @@
 VERSION=`cat package.json | underscore select '.version' --outfmt text`
 PACK_NAME=`cat package.json | underscore select '.name' --outfmt text`
 NPM_VERSION=`npm view $PACK_NAME version`
-
-if [ "$VERSION" != "$NPM_VERSION" ] && [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_EVENT_TYPE" != "push" ]
+if [ "$VERSION" != "$NPM_VERSION" ] && [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_EVENT_TYPE" != "pull_request" ]
 then
     npm publish
 else 
